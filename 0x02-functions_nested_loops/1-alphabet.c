42b16087b8bafadf0ad0a587fc1460530c4abdb6
Solomon <akusonsolomon15@gmail.com>
2022-03-16 00:20:57 +0100

    added 1-alphabet.c program

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check code
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
