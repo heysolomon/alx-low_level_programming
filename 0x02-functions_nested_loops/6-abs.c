043f3c4350cb5571ad3dd207a4f728be32845c64
Solomon <akusonsolomon15@gmail.com>
2022-03-16 14:12:51 +0100

    added 6-abs.c program

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - This is a function that prints the absolute value of an integer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int r)
+{
+	if (r > 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
